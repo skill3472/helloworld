0cb917919ae4be684062b3b1d0636d9cb027b813
Maksymilian Tym <tymszczur@gmail.com>
2022-03-03 18:05:39 +0100

    Added a C# file

---
+++ hello.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace hello
+{
+    
+    class helloworld 
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!\n");
+        }
+    }
+}
